0a05f0d7871c1ff63123a1096a86701c56a58acc
Austinor2 <austinor@ksu.edu>
2017-01-30 13:13:42 -0600

    Add project files.

---
+++ ksu.Cis501.Project 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ksu.Cis501.Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
